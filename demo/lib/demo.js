6156c76e7fe4d4f8acf2c19796759f5d3beba817
Sercan Inaler <sercan.inaler@rocket-internet.de>
2016-03-31 12:15:56 +0200

    Added regular Exp for Id Bug

--- demo/lib/demo.js
+++ demo/lib/demo.js
@@ -675,7 +675,7 @@ dataJetDemo = {
                 $.each(data.items, function(i) {
                     items +=
                         '<div class="col-md-4">' +
-                        '<a id="' + data.items[i].title.replace(' ','-').replace(' ','-') + '" href="#" class="product-link" data-toggle="modal" data-target="#modal-category">' +
+                        '<a id="' + data.items[i].title.replace(/\s+/g, '-') + '" href="#" class="product-link" data-toggle="modal" data-target="#modal-category">' +
                         '<div class="category" style="background-image: url(' + data.items[i].images[0] + ')">' +
                         '<div class="product-title" data-val="' + data.items[i].title + '">' + that.ucwords(data.items[i].title) + '</div>' +
                         '</div></a></div>';
